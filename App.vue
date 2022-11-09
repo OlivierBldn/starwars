0fae62b3ec45404df24d242ace1c93586cbd842b
OlivierBldn <95179776+OlivierBldn@users.noreply.github.com>
2022-11-09 14:36:23 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul>
+    <li v-for="character in characters" v-bind:key="character.name" >
+        {{ character.name }}
+    </li>
+  </ul>
+  
+</template>
+
+<script>
+
+
+export default {
+  data () {
+      
+    return {
+      characters: []
+    }
+  },
+  methods: {
+    getCharacters() {
+      fetch(process.env.VUE_APP_BASE_URL + process.env.VUE_APP_LIST_PATH)
+      .then(response =>response.json()
+        )
+      .then(data => {
+        this.characters = data.results
+      })
+    }
+  },
+mounted() {
+  this.getCharacters()
+},
+  
+  name: 'App',
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
